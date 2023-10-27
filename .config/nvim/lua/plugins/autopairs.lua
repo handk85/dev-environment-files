b57627b3530a04fca87b45a457fb6e84bb166a50
DongGyun Han <DongGyun.Han@rhul.ac.uk>
2023-10-27 14:36:36 +0100

    fix error

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -11,7 +11,6 @@ autopairs.setup({
     lua = { "string" }, -- don't add pairs in lua string treesitter nodes
     javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
     java = false, -- don't check treesitter on java
-    python = true,
   },
 })
 
